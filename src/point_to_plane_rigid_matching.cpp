e602f8e92bd107b1f3b9be07af9bbdeedf70dfc5
rkevingibson <rkevingibson@gmail.com>
2017-01-30 18:17:44 -0500

    First pass at the matching functions

--- src/point_to_plane_rigid_matching.cpp
+++ src/point_to_plane_rigid_matching.cpp
@@ -1,13 +1,46 @@
 #include "point_to_plane_rigid_matching.h"
-
-void point_to_point_rigid_matching(
-  const Eigen::MatrixXd & X,
-  const Eigen::MatrixXd & P,
-  const Eigen::MatrixXd & N,
-  Eigen::Matrix3d & R,
-  Eigen::RowVector3d & t)
+#include <Eigen/QR>
+void point_to_plane_rigid_matching(
+	const Eigen::MatrixXd & X,
+	const Eigen::MatrixXd & P,
+	const Eigen::MatrixXd & N,
+	Eigen::Matrix3d & R,
+	Eigen::RowVector3d & t)
 {
-  // Replace with your code
-  R = Eigen::Matrix3d::Identity();
-  t = Eigen::RowVector3d::Zero();
+	//The derivation is a mess, but problem reduces to solving a 6D linear system. 
+	//Nice and easy. 
+	int k = X.rows();
+	Eigen::MatrixXd A(3 * k, 6);
+	Eigen::VectorXd J(3 * k); //This is the collection of diagonalized normals
+	Eigen::MatrixXd K(k, 3 * k); //This is the X-P collection.
+	
+	//TODO: Create all these matrices.
+	A.setZero();
+	A.block(k, 0, k, 1) = -X.col(2);
+	A.block(2 * k, 0, k, 1) = X.col(1);
+	A.block(0, 1, k, 1) = X.col(2);
+	A.block(2 * k, 1, k, 1) = -X.col(0);
+	A.block(0, 2, k, 1) = -X.col(1);
+	A.block(k, 2, k, 1) = X.col(0);
+	A.block(0, 3, k, 1) = Eigen::VectorXd::Ones(k);
+	A.block(k, 4, k, 1) = Eigen::VectorXd::Ones(k);
+	A.block(2 * k, 5, k, 1) = Eigen::VectorXd::Ones(k);
+
+	J.segment(0, k) = X.col(0) - P.col(0);
+	J.segment(k, k) = X.col(1) - P.col(1);
+	J.segment(k * 2, k) = X.col(2) - P.col(2);
+
+	K.setZero();
+	K.block(0, 0, k, k) = Eigen::DiagonalMatrix<double, Eigen::Dynamic>(N.col(0));
+	K.block(0, k, k, k) = Eigen::DiagonalMatrix<double, Eigen::Dynamic>(N.col(1));
+	K.block(0, 2*k, k, k) = Eigen::DiagonalMatrix<double, Eigen::Dynamic>(N.col(2));
+
+	auto M = A.transpose()*K.transpose()*K*A;
+	Eigen::VectorXd u = M.colPivHouseholderQr().solve(A.transpose()*K.transpose()*K*J);
+
+	R = Eigen::Matrix3d::Zero();
+	R(1, 0) = -u(2); R(0, 1) = u(2);
+	R(2, 0) = u(1); R(0, 2) = -u(1);
+	R(1, 2) = -u(0); R(2, 1) = u(0);
+	t = u.segment<3>(3);
 }
--- src/point_to_point_rigid_matching.cpp
+++ src/point_to_point_rigid_matching.cpp
@@ -1,14 +1,26 @@
 #include "point_to_point_rigid_matching.h"
-#include <igl/polar_svd.h>
+#include "closest_rotation.h"
 
 void point_to_point_rigid_matching(
-  const Eigen::MatrixXd & X,
-  const Eigen::MatrixXd & P,
-  Eigen::Matrix3d & R,
-  Eigen::RowVector3d & t)
+	const Eigen::MatrixXd & X,
+	const Eigen::MatrixXd & P,
+	Eigen::Matrix3d & R,
+	Eigen::RowVector3d & t)
 {
-  // Replace with your code
-  R = Eigen::Matrix3d::Identity();
-  t = Eigen::RowVector3d::Zero();
+	//I'm going to do the exact optimization, following 
+	//https://content.sakai.rutgers.edu/access/content/group/7bee3f05-9013-4fc2-8743-3c5078742791/material/svd_ls_rotation.pdf
+
+	Eigen::Vector3d x_centroid = X.colwise().mean();
+	Eigen::Vector3d p_centroid = P.colwise().mean();
+	
+	Eigen::MatrixXd x_centered = X;
+	Eigen::MatrixXd p_centered = P;
+
+	x_centered.rowwise() -= x_centroid.transpose();
+	p_centered.rowwise() -= p_centroid.transpose();
+
+	Eigen::Matrix3d S = X.transpose()*P;
+	closest_rotation(S, R);
+	t = p_centroid - R*x_centroid;
 }
 
