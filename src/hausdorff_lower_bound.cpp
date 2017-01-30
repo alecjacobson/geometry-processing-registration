0df977b700ef5b5551ce54ba87886c7fa2490a12
rkevingibson <rkevingibson@gmail.com>
2017-01-30 16:34:38 -0500

    Added hausdorff lower bound computation. Super easy!

--- src/hausdorff_lower_bound.cpp
+++ src/hausdorff_lower_bound.cpp
@@ -1,12 +1,21 @@
 #include "hausdorff_lower_bound.h"
-
+#include "random_points_on_mesh.h"
+#include "point_mesh_distance.h"
 double hausdorff_lower_bound(
-  const Eigen::MatrixXd & VX,
-  const Eigen::MatrixXi & FX,
-  const Eigen::MatrixXd & VY,
-  const Eigen::MatrixXi & FY,
-  const int n)
+	const Eigen::MatrixXd & VX,
+	const Eigen::MatrixXi & FX,
+	const Eigen::MatrixXd & VY,
+	const Eigen::MatrixXi & FY,
+	const int n)
 {
-  // Replace with your code
-  return 0;
+	//Computing the lower bound of a directed hausdorff distance. 
+	//First, randomly sample the X mesh.
+	Eigen::MatrixXd samples;
+	random_points_on_mesh(n, VX, FX, samples);
+	//Find the distances to the mesh.
+
+	Eigen::VectorXd D;
+	Eigen::MatrixXd P, N;
+	point_mesh_distance(samples, VY, FY, D, P, N);
+	return D.maxCoeff();
 }
