7743d86da2c6ba9b4ac8668148b03b3ab8c14e35
Michael Tao <mtao@dgp.toronto.edu>
2017-01-30 11:44:52 -0500

    random points on mesh seems to work

--- src/random_points_on_mesh.cpp
+++ src/random_points_on_mesh.cpp
@@ -1,4 +1,7 @@
 #include "random_points_on_mesh.h"
+#include <igl/doublearea.h>
+#include <igl/cumsum.h>
+#include <map>
 
 void random_points_on_mesh(
   const int n,
@@ -9,5 +12,36 @@ void random_points_on_mesh(
   // REPLACE WITH YOUR CODE:
   X.resize(n,3);
   for(int i = 0;i<X.rows();i++) X.row(i) = V.row(i%V.rows());
+
+  Eigen::VectorXd area;
+  igl::doublearea(V,F,area);
+  area = area.array().abs() / 2;
+  Eigen::VectorXd cums;
+  igl::cumsum(area,1,cums);
+
+  cums /= cums(cums.rows() - 1);
+  std::map<double,int> gi;
+  for(int i = 0; i < cums.rows(); ++i) {
+      gi[cums(i)] = i;
+  }
+
+  auto getind = [&](double v) {
+      auto it = gi.upper_bound(v);
+      it--;
+      return it->second;
+
+  };
+
+  auto FV = [&](int t, int ind) {
+      return V.row(F(t,ind));
+  };
+
+  auto r = (Eigen::VectorXd::Random(n).array() + 1) / 2;
+    for(int i = 0; i < n; ++i) {
+        int t = getind(r(i));
+        auto pr = (Eigen::Vector2d::Random().array()+1)/2;
+        X.row(i) = (1 - pr(0) - pr(1)) * FV(t,0) + pr(0) * FV(t,1) + pr(1) * FV(t,2);
+    }
+
 }
 
