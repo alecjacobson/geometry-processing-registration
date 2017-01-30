b207f1d08383fbd70abe1b779b586042a3bdfbf6
Alec Jacobson <alecjacobson@gmail.com>
2017-01-29 21:53:58 -0500

    closest rotation function

---
+++ include/point_to_plane_rigid_matching.h
@@ -0,0 +1,25 @@
+#ifndef POINT_TO_PLANE_RIGID_MATCHING_H
+#define POINT_TO_PLANE_RIGID_MATCHING_H
+#include <Eigen/Core>
+// Given a set of source points `X` and corresponding target points `P` and
+// normals `N`, find the optimal rigid transformation (`R`,`t`) that aligns `X`
+// to `P`, minimizing the matching energy:
+//
+//   (R X' - t' 1' - P')²
+//
+// Inputs:
+//   X  #X by 3 set of source points
+//   P  #X by 3 set of target points
+//   N  #X by 3 set of target normals
+// Outputs:
+//   R  3 by 3 rotation matrix
+//   t  3d translation vector 
+//   
+void point_to_plane_rigid_matching(
+  const Eigen::MatrixXd & X,
+  const Eigen::MatrixXd & P,
+  Eigen::Matrix3d & R,
+  Eigen::RowVector3d & t);
+#endif
+
+
---
+++ src/closest_rotation.cpp
@@ -0,0 +1,9 @@
+#include "closest_rotation.h"
+
+void closest_rotation(
+  const Eigen::Matrix3d & M,
+  Eigen::Matrix3d & R)
+{
+  // Replace with your code
+  R = Eigen::Matrix3d::Identity();
+}
