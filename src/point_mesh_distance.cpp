0a2c3bd9d5d1ea40e4a9339c15890c894cee3604
leng <maryelaine.malit@gmail.com>
2017-01-31 13:49:13 -0500

    inefficient implementation of closest distance

--- src/point_mesh_distance.cpp
+++ src/point_mesh_distance.cpp
@@ -1,4 +1,6 @@
 #include "point_mesh_distance.h"
+#include "point_triangle_distance.h"
+#include <igl/per_face_normals.h>
 
 void point_mesh_distance(
   const Eigen::MatrixXd & X,
@@ -8,9 +10,31 @@ void point_mesh_distance(
   Eigen::MatrixXd & P,
   Eigen::MatrixXd & N)
 {
-  // Replace with your code
+  // Get a list of the normals corresponding to all faces in FY
+  Eigen::MatrixXd NY(FY.rows(), 3);
+  igl::per_face_normals(VY, FY, NY);
+  
+  // Find the closest projection point on the mesh for each point in X
   P.resizeLike(X);
-  N = Eigen::MatrixXd::Zero(X.rows(),X.cols());
-  for(int i = 0;i<X.rows();i++) P.row(i) = VY.row(i%VY.rows());
-  D = (X-P).rowwise().norm();
+  N.resizeLike(X);
+  double d;
+  Eigen::RowVector3d p;
+  for(int i = 0; i < X.rows(); i++) {
+    
+    // get initial values
+    point_triangle_distance(X.row(i), VY.row(FY(0,0)), VY.row(FY(0,1)), VY.row(FY(0,2)), NY.row(0), d, p);
+    D(i) = d;
+    P.row(i) = p;
+    N.row(i) = NY.row(0).normalized();
+    
+    // find better values
+    for (int j = 1; j < FY.rows(); j++) {
+      point_triangle_distance(X.row(i), VY.row(FY(j,0)), VY.row(FY(j,1)), VY.row(FY(j,2)), NY.row(j), d, p);
+      if (d < D(i)) {
+        D(i) = d;
+        P.row(i) = p;
+        N.row(i) = NY.row(j).normalized();
+      }
+    }
+  }
 }
