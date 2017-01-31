cf8274f07487107a07f16e3add08ce1b2969d128
Michael Tao <mtao@dgp.toronto.edu>
2017-01-30 19:57:53 -0500

    closest rotation seems to work

--- src/closest_rotation.cpp
+++ src/closest_rotation.cpp
@@ -1,4 +1,6 @@
 #include "closest_rotation.h"
+#include <Eigen/SVD>
+#include <Eigen/Dense>
 
 void closest_rotation(
   const Eigen::Matrix3d & M,
@@ -6,4 +8,14 @@ void closest_rotation(
 {
   // Replace with your code
   R = Eigen::Matrix3d::Identity();
+
+  Eigen::JacobiSVD<Eigen::Matrix3d> svd(M,Eigen::ComputeThinU | Eigen::ComputeThinV);
+
+  double det = (svd.matrixU() * svd.matrixV().transpose()).determinant();
+  Eigen::Matrix3d omega = Eigen::Matrix3d::Identity();
+  omega(2,2) = det;
+
+  R = svd.matrixU() * omega * svd.matrixV().transpose();
+
+
 }
