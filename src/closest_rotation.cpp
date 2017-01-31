686c9c389053cb935d4d771d6df77ccc8ae5b4d6
leng <maryelaine.malit@gmail.com>
2017-01-31 15:36:14 -0500

    impl closest_rotation

--- src/closest_rotation.cpp
+++ src/closest_rotation.cpp
@@ -1,9 +1,17 @@
 #include "closest_rotation.h"
+#include <Eigen/SVD>
+#include <Eigen/LU>
 
 void closest_rotation(
   const Eigen::Matrix3d & M,
   Eigen::Matrix3d & R)
 {
-  // Replace with your code
-  R = Eigen::Matrix3d::Identity();
+  Eigen::JacobiSVD<Eigen::Matrix3d> svd(M, Eigen::ComputeThinU | Eigen::ComputeThinV);
+  Eigen::Matrix3d uvt = svd.matrixU() * svd.matrixV().transpose();
+  double detUV = uvt.determinant();
+  Eigen::Matrix3d sigma;
+  sigma << 1, 0, 0,
+           0, 1, 0,
+           0, 0, detUV;
+  R = svd.matrixU() * sigma * svd.matrixV().transpose();
 }
