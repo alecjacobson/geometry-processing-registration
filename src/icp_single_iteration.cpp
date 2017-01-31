f888c481a0e6f1753edd3aa18a310e7c92554b67
Michael Tao <mtao@dgp.toronto.edu>
2017-01-30 19:58:23 -0500

    point to point seems to work

--- src/icp_single_iteration.cpp
+++ src/icp_single_iteration.cpp
@@ -1,4 +1,12 @@
 #include "icp_single_iteration.h"
+#include "random_points_on_mesh.h"
+#include "point_mesh_distance.h"
+#include "point_to_point_rigid_matching.h"
+#include <iostream>
+
+
+
+
 
 void icp_single_iteration(
   const Eigen::MatrixXd & VX,
@@ -10,7 +18,22 @@ void icp_single_iteration(
   Eigen::Matrix3d & R,
   Eigen::RowVector3d & t)
 {
-  // Replace with your code
-  R = Eigen::Matrix3d::Identity();
-  t = Eigen::RowVector3d::Zero();
+    R = Eigen::Matrix3d::Identity();
+    t = Eigen::RowVector3d::Zero();
+
+    Eigen::MatrixXd X;
+    random_points_on_mesh(num_samples,VX,FX,X);
+    Eigen::VectorXd D;
+    Eigen::MatrixXd P;
+    Eigen::MatrixXd N;
+
+
+    point_mesh_distance(X,VY,FY,D,P,N);
+
+    if(method == ICP_METHOD_POINT_TO_POINT ) {
+        point_to_point_rigid_matching(X,P,R,t);
+    } else if(method == ICP_METHOD_POINT_TO_PLANE ) {
+    std::cout << "NOT IMPLEMENTED: " << std::endl;
+    }
+
 }
