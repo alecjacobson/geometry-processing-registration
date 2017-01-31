2f8b6a5616e0805883e568e4648954f509ce9160
Michael Tao <mtao@dgp.toronto.edu>
2017-01-30 19:58:18 -0500

    poitn to point works up to some rotation weirdness

--- src/point_to_point_rigid_matching.cpp
+++ src/point_to_point_rigid_matching.cpp
@@ -1,5 +1,7 @@
 #include "point_to_point_rigid_matching.h"
 #include <igl/polar_svd.h>
+#include "closest_rotation.h"
+#include <iostream>
 
 void point_to_point_rigid_matching(
   const Eigen::MatrixXd & X,
@@ -10,5 +12,23 @@ void point_to_point_rigid_matching(
   // Replace with your code
   R = Eigen::Matrix3d::Identity();
   t = Eigen::RowVector3d::Zero();
+
+
+  Eigen::RowVector3d cX = X.colwise().sum() / X.rows();
+  Eigen::RowVector3d cP = P.colwise().sum() / X.rows();
+  Eigen::MatrixXd bX = X.rowwise() - cX;
+  Eigen::MatrixXd bP = P.rowwise() - cP;
+  Eigen::Matrix3d M = bP.transpose() * bX;
+
+  closest_rotation(M,R);
+
+
+
+  t = (cP -  cX * R.transpose());
+
+  //TODO: Why is this the best (R here R.T for t?
+  R = R.transpose().eval();
+  std::cout << t.transpose() << std::endl;
+
 }
 
