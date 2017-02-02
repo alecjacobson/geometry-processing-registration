dd889f97619aba632b88410bc676cb6b8c1e9cd0
leng <maryelaine.malit@gmail.com>
2017-02-02 13:15:55 -0500

    point to plane moving in the wrong direction???

--- src/point_to_plane_rigid_matching.cpp
+++ src/point_to_plane_rigid_matching.cpp
@@ -1,6 +1,7 @@
 #include "point_to_plane_rigid_matching.h"
 #include "closest_rotation.h"
 #include <Eigen/Dense>
+#include <iostream>
 
 void point_to_plane_rigid_matching(
   const Eigen::MatrixXd & X,
@@ -12,29 +13,31 @@ void point_to_plane_rigid_matching(
   // alpha, beta, gamma, tx, ty, tz
   Eigen::VectorXd u(6);
   
-  // 6x6 Covariance matrix
-  Eigen::MatrixXd C(6,6);
-  C.setZero();
+  // kx6 matrix A
+  int k = X.rows();
+  Eigen::MatrixXd A(k,6);
   
-  // 6x1 b array
-  Eigen::VectorXd b(6);
-  b.setZero();
+  // k vector b
+  Eigen::VectorXd b(k);
   
-  Eigen::Vector3d c, x, n;
-  Eigen::VectorXd cn(6);
-  double xpn;
-  for (int i = 0; i < X.rows(); i++) {
+  // Construct A and b
+  Eigen::Vector3d x, n, c, p;
+  for (int i = 0; i < k; i++) {
     x = X.row(i);
     n = N.row(i);
+    p = P.row(i);
     c = x.cross(n);
-    cn << c(0), c(1), c(2), N(i,0), N(i,1), N(i,2);
-    C = C + (cn * cn.transpose());
-    xpn = (X.row(i) - P.row(i)).dot(N.row(i));
-    b = b + (xpn * cn);
+    A(i,0) = c(0);
+    A(i,1) = c(1);
+    A(i,2) = c(2);
+    A(i,3) = n(0);
+    A(i,4) = n(1);
+    A(i,5) = n(2);
+    b(i) = (x-p).dot(n);
   }
-  
-  // Solve for Cu = -b;
-  u = C.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(-1 * b);
+
+  // Solve for Ax = b;
+  u = (A).jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(-1* b);
   
   // Retrieve rotation matrix: alpha=u(0), beta=u(1), gamma=u(2)
   Eigen::MatrixXd M(3,3);
@@ -45,4 +48,5 @@ void point_to_plane_rigid_matching(
   
   // Assign values of t
   t << u(3), u(4), u(5);
+  std::cout << t << std::endl;
 }
