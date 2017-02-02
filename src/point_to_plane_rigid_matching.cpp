931974708f408cfd99f31ed452f0b5425ecabbfe
leng <maryelaine.malit@gmail.com>
2017-02-02 17:42:50 -0500

    point-to-plane still weird

--- src/point_to_plane_rigid_matching.cpp
+++ src/point_to_plane_rigid_matching.cpp
@@ -10,43 +10,36 @@ void point_to_plane_rigid_matching(
   Eigen::Matrix3d & R,
   Eigen::RowVector3d & t)
 {
-  // alpha, beta, gamma, tx, ty, tz
+  // u: alpha, beta, gamma, t1, t2, t3
   Eigen::VectorXd u(6);
   
-  // kx6 matrix A
-  int k = X.rows();
-  Eigen::MatrixXd A(k,6);
+  Eigen::MatrixXd C(6,6);
+  Eigen::VectorXd b(6);
+  C.setZero();
+  b.setZero();
   
-  // k vector b
-  Eigen::VectorXd b(k);
-  
-  // Construct A and b
-  Eigen::Vector3d x, n, c, p;
-  for (int i = 0; i < k; i++) {
+  Eigen::Vector3d x, p, n, c;
+  Eigen::VectorXd cov(6);
+  for (int i = 0; i < X.rows(); i++) {
     x = X.row(i);
     n = N.row(i);
     p = P.row(i);
     c = x.cross(n);
-    A(i,0) = c(0);
-    A(i,1) = c(1);
-    A(i,2) = c(2);
-    A(i,3) = n(0);
-    A(i,4) = n(1);
-    A(i,5) = n(2);
-    b(i) = (x-p).dot(n);
+    cov << c(0), c(1), c(2), n(0), n(1), n(2);
+    C += cov * cov.transpose();
+    b -= (x-p).dot(n) * cov;
   }
-
-  // Solve for Ax = b;
-  u = (A).jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(-1* b);
   
-  // Retrieve rotation matrix: alpha=u(0), beta=u(1), gamma=u(2)
-  Eigen::MatrixXd M(3,3);
+  // Solve for Cu = b
+  u = C.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(b);
+  
+  // Construct M, R
+  Eigen::Matrix3d M;
   M << 0, -u(2), u(1),
        u(2), 0, -u(0),
        -u(1), u(0), 0;
-  closest_rotation(M, R);
+  closest_rotation(M,R);
   
-  // Assign values of t
+  // Construct t
   t << u(3), u(4), u(5);
-  std::cout << t << std::endl;
 }
