a2127ae0c76928a67526104e835dbe44906e4356
tianhao-david-hu <tianhao.david.hu@gmail.com>
2018-02-02 12:05:10 -0500

    closest_rotation implemented but not verified.

--- src/closest_rotation.cpp
+++ src/closest_rotation.cpp
@@ -1,9 +1,28 @@
 #include "closest_rotation.h"
+#include <iostream>
+#include <Eigen/Dense>
+#include <Eigen/SVD>
 
+using namespace Eigen;
+// Given a 3¡Á3 matrix `M`, find the closest rotation matrix `R`.
+//
+// Inputs:
+//   M  3x3 matrix
+// Outputs:
+//   R  3x3 rotation matrix
+//
+//we should project M onto the space of rotation matrices
 void closest_rotation(
   const Eigen::Matrix3d & M,
   Eigen::Matrix3d & R)
 {
-  // Replace with your code
-  R = Eigen::Matrix3d::Identity();
+  JacobiSVD<Matrix3d> svd(M, DecompositionOptions::ComputeThinU | DecompositionOptions::ComputeThinV);
+  Matrix3d U = svd.matrixU();
+  Matrix3d V = svd.matrixV();
+  Matrix3d Omega = Matrix3d::Identity();
+  double UV_det = (U*V.transpose()).determinant();
+  if (UV_det)
+    Omega(2, 0) = -1;
+  Omega(2, 2) = UV_det;
+  R = U*Omega*V.transpose();
 }
