2c7417fac8ce87bd62578c1af2432eea5fdc2528
mianwei <mianwei@mianweimacbook.local>
2018-02-03 22:54:26 -0500

    Point to Plane left to do

--- src/point_to_point_rigid_matching.cpp
+++ src/point_to_point_rigid_matching.cpp
@@ -1,6 +1,7 @@
 #include "point_to_point_rigid_matching.h"
 #include <igl/polar_svd.h>
-
+#include "closest_rotation.h"
+#include <Eigen/Dense>
 void point_to_point_rigid_matching(
   const Eigen::MatrixXd & X,
   const Eigen::MatrixXd & P,
@@ -8,7 +9,23 @@ void point_to_point_rigid_matching(
   Eigen::RowVector3d & t)
 {
   // Replace with your code
-  R = Eigen::Matrix3d::Identity();
-  t = Eigen::RowVector3d::Zero();
+    Eigen::MatrixXd XBar, PBar, M;
+    M.resize(3,3);
+    XBar.resizeLike(X);
+    XBar = X;
+    PBar.resizeLike(P);
+    PBar = P;
+    double xNum, pNum;
+    xNum = X.rows();
+    pNum = P.rows();
+    
+    for (int i = 0; i < 3; i ++) {
+        XBar.col(i).array() -= (XBar.col(i).mean());
+        PBar.col(i).array() -= (PBar.col(i).mean());
+    }
+    M =   (XBar.transpose()) * PBar;
+    
+    closest_rotation(M,R);
+    t = P.colwise().sum().transpose() - R * (P.colwise().sum().transpose());
 }
 
