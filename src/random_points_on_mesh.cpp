eab964527f7995ea44cd5118b6f1e693d17442b7
zero-impact <lawsonfulton@gmail.com>
2018-02-04 20:42:07 -0500

    random points

--- src/random_points_on_mesh.cpp
+++ src/random_points_on_mesh.cpp
@@ -1,5 +1,8 @@
 #include "random_points_on_mesh.h"
 
+#include <igl/doublearea.h>
+#include <igl/cumsum.h>
+
 void random_points_on_mesh(
   const int n,
   const Eigen::MatrixXd & V,
@@ -8,6 +11,36 @@ void random_points_on_mesh(
 {
   // REPLACE WITH YOUR CODE:
   X.resize(n,3);
-  for(int i = 0;i<X.rows();i++) X.row(i) = V.row(i%V.rows());
+
+  Eigen::VectorXd A(F.rows());
+  igl::doublearea(V, F, A);
+  double A_x = A.sum();
+  Eigen::VectorXd C(F.rows());
+  igl::cumsum(A/A_x, 1, C);
+
+  for(int i = 0; i < n; i++) {
+  	double gamma = ((double) rand() / (RAND_MAX)) + 1;
+  	// Pick a triangle
+  	int tri_idx;
+  	for(int j = 0; j < n; j++) { // TODO use binary search?
+  		if(gamma <= C[j]) {
+  			tri_idx = j;
+  		}
+  	}
+  	// Pick a point
+  	double alpha = ((double) rand() / (RAND_MAX)) + 1;
+  	double beta = ((double) rand() / (RAND_MAX)) + 1;
+
+  	if(alpha + beta > 1) {
+  		alpha = 1.0 - alpha;
+  		beta = 1.0 - beta;
+  	}
+
+  	Eigen::RowVector3d v1 = V.row(F(tri_idx, 0));
+  	Eigen::RowVector3d v2 = V.row(F(tri_idx, 1));
+  	Eigen::RowVector3d v3 = V.row(F(tri_idx, 2));
+
+  	X.row(i) = v1 + alpha * (v2 - v1) + beta * (v3 - v1);
+  }
 }
 
