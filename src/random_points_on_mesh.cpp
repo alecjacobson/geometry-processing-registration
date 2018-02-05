1fdc52c707c8e89d87f3966737139360d8b809ab
zero-impact <lawsonfulton@gmail.com>
2018-02-05 13:26:06 -0500

    Random points working

--- src/random_points_on_mesh.cpp
+++ src/random_points_on_mesh.cpp
@@ -1,5 +1,8 @@
 #include "random_points_on_mesh.h"
 
+#include <iostream>
+#include <random>
+
 #include <igl/doublearea.h>
 #include <igl/cumsum.h>
 
@@ -11,25 +14,32 @@ void random_points_on_mesh(
 {
   // REPLACE WITH YOUR CODE:
   X.resize(n,3);
+  
+  std::default_random_engine generator;
+  std::uniform_real_distribution<double> distribution(0.0,1.0);
 
   Eigen::VectorXd A(F.rows());
   igl::doublearea(V, F, A);
+  A = A / 2.0;
   double A_x = A.sum();
   Eigen::VectorXd C(F.rows());
   igl::cumsum(A/A_x, 1, C);
-
+  // std::cout << "C: " << C << std::endl;
   for(int i = 0; i < n; i++) {
-  	double gamma = ((double) rand() / (RAND_MAX)) + 1;
+  	double gamma = distribution(generator);
   	// Pick a triangle
-  	int tri_idx;
-  	for(int j = 0; j < n; j++) { // TODO use binary search?
-  		if(gamma <= C[j]) {
+  	int tri_idx = 0;
+  	for(int j = 0; j < C.size(); j++) { // TODO use binary search?
+  		if(C[j] > gamma) {
   			tri_idx = j;
+            break;
   		}
-  	}
+    }
+    // std::cout<< "gamma: " << gamma << std::endl;
+    // std::cout<< "index: " << tri_idx << std::endl;
   	// Pick a point
-  	double alpha = ((double) rand() / (RAND_MAX)) + 1;
-  	double beta = ((double) rand() / (RAND_MAX)) + 1;
+  	double alpha = distribution(generator);
+  	double beta = distribution(generator);
 
   	if(alpha + beta > 1) {
   		alpha = 1.0 - alpha;
