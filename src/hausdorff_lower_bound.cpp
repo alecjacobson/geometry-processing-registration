581d3e4e84b02cbf4a4ee3b826965bba2ea7547b
Ziheng Liang <ziheng941008@hotmail.com>
2018-02-05 21:00:33 -0500

    finished src/hausdorff_lower_bound.cpp

--- src/hausdorff_lower_bound.cpp
+++ src/hausdorff_lower_bound.cpp
@@ -1,4 +1,6 @@
 #include "hausdorff_lower_bound.h"
+#include "random_points_on_mesh.h"
+#include "point_mesh_distance.h"
 
 double hausdorff_lower_bound(
   const Eigen::MatrixXd & VX,
@@ -8,5 +10,11 @@ double hausdorff_lower_bound(
   const int n)
 {
   // Replace with your code
-  return 0;
+  Eigen::MatrixXd sample;
+  random_points_on_mesh(n, VX, FX, sample);
+  Eigen::VectorXd D;
+  Eigen::MatrixXd P;
+  Eigen::MatrixXd N;
+  point_mesh_distance(sample, VY, FY, D, P, N);
+  return D.maxCoeff();
 }
