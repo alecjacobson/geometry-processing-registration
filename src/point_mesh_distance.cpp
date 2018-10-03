fb73061bf61a2190e9f8007ff1dce12bb5700d78
Mark <peiqi1122@gmail.com>
2018-10-03 03:50:32 -0400

    add

--- src/point_mesh_distance.cpp
+++ src/point_mesh_distance.cpp
@@ -1,4 +1,10 @@
 #include "point_mesh_distance.h"
+#include "point_triangle_distance.h"
+
+#include <igl/per_face_normals.h>
+#include <limits>
+#include <vector>
+#include <algorithm>
 
 void point_mesh_distance(
   const Eigen::MatrixXd & X,
@@ -8,9 +14,47 @@ void point_mesh_distance(
   Eigen::MatrixXd & P,
   Eigen::MatrixXd & N)
 {
-  // Replace with your code
-  P.resizeLike(X);
-  N = Eigen::MatrixXd::Zero(X.rows(),X.cols());
-  for(int i = 0;i<X.rows();i++) P.row(i) = VY.row(i%VY.rows());
-  D = (X-P).rowwise().norm();
+    D.resize(X.rows());
+    P.resizeLike(X);
+    N.resizeLike(X);
+
+    auto nx = X.rows();
+    auto ny = FY.rows();
+
+    Eigen::MatrixXd NY;
+    igl::per_face_normals(VY, FY, Eigen::Vector3d(1,1,1).normalized(), NY);
+
+    int min_idx;
+    double d;
+    Eigen::RowVector3d p;
+    Eigen::VectorXd closest_distances(ny);
+    Eigen::MatrixXd closest_points(ny, 3);
+
+    for (int i = 0; i < nx; ++i) {
+        auto x = X.row(i);
+
+        // Computes closest distance & points from `x` to every triangle `t`
+        for (int j = 0; j < ny; ++j) {
+            auto t = FY.row(j);
+            point_triangle_distance(x, VY.row(t(0)), VY.row(t(1)), VY.row(t(2)), d, p);
+            closest_distances(j) = d;
+            closest_points.row(j) = p;
+        }
+        
+        // Find triangle that minimizes point-to-mesh distance
+        d = std::numeric_limits<int>::max();
+        min_idx = 0;
+        for (int j = 0; j < ny; ++j) {
+            if (closest_distances(j) < d) {
+                d = closest_distances(j);
+                min_idx = j;
+            }
+        }
+        
+        // Saves closest distance, closest point, and normal
+        D(i) = d;
+        P.row(i) = closest_points.row(min_idx);
+        N.row(i) = NY.row(min_idx);
+    }
 }
+
--- src/point_triangle_distance.cpp
+++ src/point_triangle_distance.cpp
@@ -1,5 +1,8 @@
 #include "point_triangle_distance.h"
 
+#include <Eigen/Geometry> 
+
+
 void point_triangle_distance(
   const Eigen::RowVector3d & x,
   const Eigen::RowVector3d & a,
@@ -8,7 +11,67 @@ void point_triangle_distance(
   double & d,
   Eigen::RowVector3d & p)
 {
-  // Replace with your code
-  d = 0;
-  p = a;
+    // Reference 
+    //  http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.479.8237&rep=rep1&type=pdf
+
+
+    // Find point `q` on plane defined by a point `p` and normal `n` {z: (z-p) * n = 0}
+    //      such that `q` is the (orthogonally) projected point of `x`
+    auto project = [](
+        const Eigen::RowVector3d& p,
+        const Eigen::RowVector3d& n,
+        const Eigen::RowVector3d& x) {
+            return x - ((x - p).dot(n) / (n.squaredNorm())) * n;
+        };
+
+    auto n = (b-a).cross(c-a);
+    p = project(a, n, x);
+
+    // Find line connecting center of triangle and the vertices
+    auto va = (a-c).normalized() + (a-b).normalized();
+    auto vb = (b-c).normalized() + (b-a).normalized();
+    auto vc = (c-b).normalized() + (c-a).normalized();
+
+    // Find location of projected point `p`
+    //  In particular, `p` is closest to edge represented by `uv`
+    auto fa = (va.cross(p-a)).dot(n);
+    auto fb = (vb.cross(p-b)).dot(n);
+    auto fc = (vc.cross(p-c)).dot(n);
+
+    int ui, vi;
+    if (fa > 0 && fb < 0) {
+        ui = 0; vi = 1;
+    } else if (fb > 0 && fc < 0) {
+        ui = 1; vi = 2;
+    } else if (fc > 0 && fa < 0) {
+        ui = 2; vi = 0;
+    }
+
+    auto get = [&](int i){ return (i == 0) ? a : ((i == 1) ? b : c); };
+    auto u = get(ui);
+    auto v = get(vi);
+
+    if ((v-p).cross(u-p).dot(n) > 0) {
+        // project `p` to the closest edge `uv` if `p` outside triangle
+        //      the closest point is either a projected point `q` on edge `uv`
+        //      or the vertices `u` or `v`
+        auto n2 = ((v-p).cross(u-p)).cross(u-v);
+        auto q = project(u, n2, p);
+        auto t = (q-u).dot(v-u) / (v-u).dot(v-u);
+
+        if (t >= 0 && t <= 1) {
+            d = sqrt((p-q).squaredNorm() + (x-p).squaredNorm());
+            p = q;
+        } else if (t < 0) {
+            d = sqrt((p-u).squaredNorm() + (x-p).squaredNorm());
+            p = u;
+        } else {
+            d = sqrt((p-v).squaredNorm() + (x-p).squaredNorm());
+            p = v;
+        }
+    } else {
+        // `p` is the closest point on the triangle if `p` inside triangle
+        d = (x-p).norm();
+    }
+
 }
