d804fecd2f25154b2143827bd806df2e37a9ca93
liupeiLu <liupei.lu@mail.utoronto.ca>
2018-10-08 23:57:33 -0400

    Liupei Lu HW2

--- src/point_to_point_rigid_matching.cpp
+++ src/point_to_point_rigid_matching.cpp
@@ -1,14 +1,34 @@
 #include "point_to_point_rigid_matching.h"
 #include <igl/polar_svd.h>
+#include "closest_rotation.h"
+#include <Eigen/Dense>
+#include <iostream>
 
 void point_to_point_rigid_matching(
-  const Eigen::MatrixXd & X,
-  const Eigen::MatrixXd & P,
-  Eigen::Matrix3d & R,
-  Eigen::RowVector3d & t)
+    const Eigen::MatrixXd &X,
+    const Eigen::MatrixXd &P,
+    Eigen::Matrix3d &R,
+    Eigen::RowVector3d &t)
 {
-  // Replace with your code
-  R = Eigen::Matrix3d::Identity();
-  t = Eigen::RowVector3d::Zero();
-}
+  Eigen::VectorXd ones = Eigen::VectorXd::Ones(X.rows());
+  Eigen::VectorXd zeros = Eigen::VectorXd::Zero(X.rows());
 
+  Eigen::VectorXd B(X.rows() * 3);
+  B << X.col(0) - P.col(0), X.col(1) - P.col(1), X.col(2) - P.col(2);
+
+  Eigen::MatrixXd A(3 * X.rows(), 6);
+  A << zeros, X.col(2), -X.col(1), ones, zeros, zeros,
+      -X.col(2), zeros, X.col(0), zeros, ones, zeros,
+      X.col(1), -X.col(0), zeros, zeros, zeros, ones;
+
+  Eigen::VectorXd u = (A.transpose() * A).inverse() * (-A.transpose() * B);
+
+  Eigen::Matrix3d M;
+  M << 1, -u(2), u(1),
+      u(2), 1, -u(0),
+      -u(1), u(0), 1;
+
+  closest_rotation(M, R);
+
+  t << u(3), u(4), u(5);
+}
--- src/point_triangle_distance.cpp
+++ src/point_triangle_distance.cpp
@@ -1,14 +1,83 @@
 #include "point_triangle_distance.h"
+#include <Eigen/Geometry>
+
+void point_on_line(const Eigen::RowVector3d &x,
+                   const Eigen::RowVector3d &a,
+                   const Eigen::RowVector3d &b,
+                   const Eigen::RowVector3d &projectP,
+                   double &d,
+                   Eigen::RowVector3d &p)
+{
+  Eigen::RowVector3d nab = ((b - projectP).cross(a - projectP)).cross(b - a);
+  nab.normalize();
+  double d_ab = abs((a - projectP).dot(nab));
+  Eigen::RowVector3d pab = projectP + d_ab * nab;
+  double t = (pab - a).dot(b - a) / (b - a).dot(b - a);
+  if (t < 0)
+  {
+    p = a;
+    d = (x - p).norm();
+  }
+  if (0 < t < 1)
+  {
+    p = pab;
+    d = (x - p).norm();
+  }
+  if (t > 1)
+  {
+    p = b;
+    d = (x - p).norm();
+  }
+}
 
 void point_triangle_distance(
-  const Eigen::RowVector3d & x,
-  const Eigen::RowVector3d & a,
-  const Eigen::RowVector3d & b,
-  const Eigen::RowVector3d & c,
-  double & d,
-  Eigen::RowVector3d & p)
+    const Eigen::RowVector3d &x,
+    const Eigen::RowVector3d &a,
+    const Eigen::RowVector3d &b,
+    const Eigen::RowVector3d &c,
+    double &d,
+    Eigen::RowVector3d &p)
 {
-  // Replace with your code
-  d = 0;
-  p = a;
+  Eigen::RowVector3d v1 = b - a;
+  Eigen::RowVector3d v2 = -a + c;
+  Eigen::RowVector3d n = v1.cross(v2);
+  n.normalize();
+
+  double distance = abs((x - a).dot(n));
+  Eigen::RowVector3d projectP = x - distance * n;
+  Eigen::RowVector3d v3 = projectP - a;
+  Eigen::RowVector3d v4, v5, v6;
+
+  // based on https://math.stackexchange.com/questions/544946/determine-if-projection-of-3d-point-onto-plane-is-within-a-triangle
+  double u = (v2.dot(v2) * v3.dot(v1) - v1.dot(v2) * v3.dot(v2)) * 1.0 /
+             (v2.dot(v2) * v1.dot(v1) - v1.dot(v2) * v1.dot(v2));
+
+  double v = (v1.dot(v1) * v3.dot(v2) - v1.dot(v2) * v3.dot(v1)) * 1.0 /
+             (v2.dot(v2) * v1.dot(v1) - v1.dot(v2) * v1.dot(v2));
+
+  if (u >= 0 && v >= 0 && (u + v <= 1))
+  { //inside
+    p = a + u * v1 + v * v2;
+    d = distance;
+  }
+  else if (u >= 0 and v < 0)
+  {
+    // On ab
+    point_on_line(x, a, b, projectP, d, p);
+  }
+  else if (u < 0 && v >= 0)
+  {
+    //On ac
+    point_on_line(x, a, c, projectP, d, p);
+  }
+  else if (u >= 0 && v >= 0 && u + v > 1)
+  {
+    //On bc
+    point_on_line(x, b, c, projectP, d, p);
+  }
+  else
+  { //Outside
+    p = a;
+    d = (x - p).norm();
+  }
 }
