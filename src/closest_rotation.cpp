480fd1f436b97bf4bfc696189324a00e78c01873
jnyao <43320994+jnyao@users.noreply.github.com>
2018-10-08 20:08:30 -0400

    Update closest_rotation.cpp

--- src/closest_rotation.cpp
+++ src/closest_rotation.cpp
@@ -1,4 +1,9 @@
 #include "closest_rotation.h"
+#include <Eigen/SVD>
+#include <Eigen/Dense>
+#include <iostream>
+
+using namespace Eigen;
 
 void closest_rotation(
   const Eigen::Matrix3d & M,
@@ -6,4 +11,12 @@ void closest_rotation(
 {
   // Replace with your code
   R = Eigen::Matrix3d::Identity();
+  JacobiSVD<MatrixXd> svd(M, ComputeThinU | ComputeThinV);
+  Matrix3d omega;
+  omega.setZero();
+  omega(0, 0) = 1;
+  omega(1, 1) = 1;
+  omega(2, 2) = (svd.matrixU() * svd.matrixV().transpose()).determinant();
+  //std::cout<<"det: "<<omega(2,2)<<std::endl;
+  R = svd.matrixU() * omega * svd.matrixV().transpose();
 }
