b20fa23e96fec7ae41c3b4522db69137d9d986fe
rgh000 <35755112+rgh000@users.noreply.github.com>
2018-10-09 00:53:28 -0400

    fixed memory error. please consider new version

--- src/point_to_plane_rigid_matching.cpp
+++ src/point_to_plane_rigid_matching.cpp
@@ -10,18 +10,18 @@ void point_to_plane_rigid_matching(
   Eigen::RowVector3d & t)
 {
     int r = X.rows();
-    Eigen::MatrixXd A;
+    Eigen::MatrixXd A(r * 3, 6);
     A << Eigen::VectorXd::Zero(r), X.col(2), -X.col(1), Eigen::VectorXd::Ones(r), Eigen::VectorXd::Zero(r), Eigen::VectorXd::Zero(r), -X.col(2), Eigen::VectorXd::Zero(r), X.col(0), Eigen::VectorXd::Zero(r), Eigen::VectorXd::Ones(r), Eigen::VectorXd::Zero(r), X.col(1), -X.col(0), Eigen::VectorXd::Zero(r), Eigen::VectorXd::Zero(r), Eigen::VectorXd::Zero(r), Eigen::VectorXd::Ones(r);
-    Eigen::VectorXd B;
+    Eigen::VectorXd B(r * 3);
     B << X.col(0) - P.col(0), X.col(1) - P.col(1), X.col(2) - P.col(2);
-    Eigen::MatrixXd Nd;
-    Nd.block(0, 0, r, r) = N.col(0).asDiagonal();
-    Nd.block(0, r, r, r) = N.col(1).asDiagonal();
-    Nd.block(0, 2 * r, r, r) = N.col(2).asDiagonal();
+    Eigen::MatrixXd Nd(r, 3 * r);
+    Eigen::MatrixXd Nd0[] = {N.col(0).asDiagonal(), N.col(1).asDiagonal(), N.col(2).asDiagonal()};
+    Nd << Nd0[0], Nd0[1], Nd0[2];
     A = Nd * A;
     B = Nd * B;
-    Eigen::VectorXd u = (A.transpose() * A).inverse() * (-A.transpose() * B);
-    Eigen::Matrix3d M;
+    Eigen::VectorXd u;
+    u = (A.transpose() * A).inverse() * (-A.transpose() * B);
+    Eigen::Matrix3d M(3, 3);
     M <<  1, u(2), -u(1), -u(2), 1, u(0), u(1), -u(0), 1;
     closest_rotation(M,R);
     t = Eigen::Vector3d(u(3), u(4), u(5));
