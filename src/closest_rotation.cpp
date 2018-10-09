20e92a425493324cff01a2910564971070ea22bd
Randy Hickey <randyhickey@Randy.local>
2018-10-08 23:45:04 -0400

    done

--- src/closest_rotation.cpp
+++ src/closest_rotation.cpp
@@ -1,9 +1,15 @@
 #include "closest_rotation.h"
+#include <Eigen/Dense>
+#include <Eigen/LU>
 
 void closest_rotation(
   const Eigen::Matrix3d & M,
   Eigen::Matrix3d & R)
 {
-  // Replace with your code
-  R = Eigen::Matrix3d::Identity();
+    Eigen::JacobiSVD<Eigen::MatrixXd> svd(M, Eigen::ComputeThinU | Eigen::ComputeThinV);
+    Eigen::Matrix3d U = svd.matrixU();
+    Eigen::Matrix3d Vt = (svd.matrixV()).transpose();
+    Eigen::Matrix3d O = Eigen::Matrix3d::Identity();
+    O(2,2) = (U * Vt).determinant();
+    R = U * O * Vt;
 }
--- src/point_mesh_distance.cpp
+++ src/point_mesh_distance.cpp
@@ -1,4 +1,6 @@
 #include "point_mesh_distance.h"
+#include "point_triangle_distance.h"
+#include <igl/per_face_normals.h>
 
 void point_mesh_distance(
   const Eigen::MatrixXd & X,
@@ -8,9 +10,26 @@ void point_mesh_distance(
   Eigen::MatrixXd & P,
   Eigen::MatrixXd & N)
 {
-  // Replace with your code
-  P.resizeLike(X);
-  N = Eigen::MatrixXd::Zero(X.rows(),X.cols());
-  for(int i = 0;i<X.rows();i++) P.row(i) = VY.row(i%VY.rows());
-  D = (X-P).rowwise().norm();
+    P.resizeLike(X);
+    N = Eigen::MatrixXd::Zero(X.rows(),X.cols());
+    D.resize(X.rows());
+    Eigen::MatrixXd normals;
+    igl::per_face_normals(VY, FY, Eigen::Vector3d(1,1,1).normalized(), normals);
+    for(int i = 0;i<X.rows();i++) {
+        double minD = 100000000;
+        double d_;
+        int tri;
+        Eigen::RowVector3d p_, minP;
+        for (int j = 0; j < FY.rows(); j++) {
+            point_triangle_distance(X.row(i), VY.row(FY(j, 0)), VY.row(FY(j, 1)), VY.row(FY(j, 2)), d_, p_);
+            if (d_ < minD) {
+                minD = d_;
+                minP = p_;
+                tri = j;
+            }
+        }
+        P.row(i) = minP;
+        N.row(i) = normals.row(tri);
+        D(i) = minD;
+    }
 }
